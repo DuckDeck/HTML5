20d5c0b0172e24d99bb40ce1fb5b595916c53896
xiaoman <123456@qq.com>
2016-10-12 23:06:53 +0800

    editer,but it can not work until now

---
+++ js/base.js
@@ -0,0 +1,20 @@
+function Editor(selector) {
+    this.$editer = $(selector)
+    this.bindEvents_()
+}
+
+Editor.prototype.bindEvents_ = function() {
+    this.$editer.on('click', 'button', function(e) {
+        switch (e.currentTarget.id) {
+            case 'bold':
+            document.execCommand('bold')
+            break
+            case 'italic':
+            document.execCommand('italic')
+            break
+            case 'underline':
+            document.execCommand('underline')
+            break
+        }
+    })
+}
