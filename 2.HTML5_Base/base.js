55f0a7dab7126559fd99e8bc27169ff4905b4b68
xiaoman <123456@qq.com>
2016-10-12 22:24:30 +0800

    zzzz

--- 2.HTML5_Base/base.js
+++ 2.HTML5_Base/base.js
@@ -2,3 +2,11 @@ function Editor(selector) {
     this.$editer = $(selector)
     this.bindEvents_()
 }
+
+Editor.prototype.bindEvents_ = function() {
+    this.$editer.on('click', 'button', function(e) {
+        switch (e.currentTarget.id) {
+
+        }
+    })
+}
