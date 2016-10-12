89efa6b40858efcea0c65dc4c07395b7b7e530f5
xiaoman <123456@qq.com>
2016-10-12 23:30:23 +0800

    11111

--- js/base.js
+++ js/base.js
@@ -7,14 +7,14 @@ Editor.prototype.bindEvents_ = function() {
     this.$editer.on('click', 'button', function(e) {
         switch (e.currentTarget.id) {
             case 'bold':
-            document.execCommand('bold')
-            break
+                document.execCommand('bold')
+                break
             case 'italic':
-            document.execCommand('italic')
-            break
+                document.execCommand('italic')
+                break
             case 'underline':
-            document.execCommand('underline')
-            break
+                document.execCommand('underline');
+                break
         }
     })
 }
