a069ac4295df94e2811257cee72fee9778dce446
stan <3421902@qq.com>
2016-10-24 20:51:36 +0800

    123

---
+++ 5.touch/touch.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    var el = document.getElementById('touch-box')
+    Hammer(el).on('swipeleft',function(e){
+        alert("tou swipe to left success")
+    })
+
+
+var hammertime = Hammer(document.getElementById('pinchzoom'),{
+    transform_always_block:true,
+    transform_min_scale:0.5,
+    drag_block_horizontal:true,
+    drag_block_vertical:true,
+    drag_min_distance:0
+})
+var img = document.getElementById('img')
+var posX = 0,
+posY = 0,
+scale = 1,
+last_scale,
+rotation = 1,
+last_rocation
+
+hammertime.on('touch drag transform',function(e){
+    switch(e.type){
+        case 'touch':
+        last_scale = scale
+        last_rocation = rotation
+        last_posX = posX
+        last_posY = posY
+        break
+        case 'drag':
+        posX = last_posX + e.gesture.deltaX
+        posY = last_posY + e.gesture.deltaY
+        break
+        case 'transform':
+        rotation = last_rocation + e.gesture.rotation
+        scale = Math.min(last_scale * gesture.scale,10)
+        break
+    }
+var transform = "translate3d("+posX +"px,"+posY+"px,0)"+
+"scale3d("+scale +","+scale +",0)"+
+"rotate("+rotation +"deg)"
+img.style.transform = transform
+})
+
+})
--- js/base.js
+++ js/base.js
@@ -21,3 +21,53 @@ Editor.prototype.bindEvents_ = function() {
         }
     })
 }
+
+
+
+  $(document).ready(function(){
+                if (Modernizr.draganddrop){
+                    console.log("this boswer suppert drag")
+                }
+                else{
+                    console.log("this broswer can not support drag")
+                }
+                function handleDragstart(e){
+                    this.style.opacity = '0.2'
+                }
+                function handleDragOver(e){
+                    if(e.preventDefault){
+                        e.preventDefault()
+                    }
+                    e.dataTransfer.dropEffect = 'move'
+                    return false
+                }
+                function handerDragEnter(e){
+                    this.classList.add('over')
+                }
+                function handleDragLeave(e){
+                    this.classList.remove('over')
+                }
+                function handleDrag(e){
+                   console.log('drag',this)
+                }
+                 function handleDrop(e){
+                    if(e.stopPropagation){
+                        e.stopPropagation()
+                    }
+                    console.log(this)
+                    return false
+                }
+                function handleDragEnd(e){
+                    console.log(this)
+                    $('.box').each(function(){
+                        $(this).classList.remove('over')
+                    })
+                }
+                $('.box').each(function(){
+                  //  $(this)[0].addEventListener('dragstart',handleDragstart,false)
+                  $(this).bind('dragstart',handleDragstart)
+                   $(this).bind('dragenter',handerDragEnter)
+                    $(this).bind('dragover',handleDragOver)
+                     $(this).bind('dragleave',handleDragLeave)
+            })
+})
